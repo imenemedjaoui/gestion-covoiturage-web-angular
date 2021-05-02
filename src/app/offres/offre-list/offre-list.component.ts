b102888cbef1a5d0e0d1521b13405937323106df
ImeneMina <ImeneMedjaoui@users.noreply.github.com>
2021-05-02 15:07:14 +0100

    commit

--- src/app/offres/offre-list/offre-list.component.ts
+++ src/app/offres/offre-list/offre-list.component.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Store } from '@ngrx/store';
+import { Store,select } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
+import * as fromOffre from "../state/offre.reducer";
+import { Offre } from "../offre.model";
 import * as offreActions from '../state/offre.actions';
+
 @Component({
   selector: 'app-offre-list',
   templateUrl: './offre-list.component.html',
   styleUrls: ['./offre-list.component.css']
 })
 export class OffreListComponent implements OnInit {
-  offres;
-  constructor(private store: Store<any>) { }
+  offres$: Observable<Offre[]>;
+  error$: Observable<String>;
+  constructor(private store: Store<fromOffre.AppState>) { }
 
   ngOnInit() {
     this.store.dispatch(new offreActions.LoadOffres());
-    this.store.subscribe(state => (this.offres = state.offres.offres));
+    this.offres$ = this.store.pipe(select(fromOffre.getOffres));
+    this.error$ = this.store.pipe(select(fromOffre.getError));
   }
 
+deleteOffre(offre:  Offre) {
+    if (confirm("Êtes-vous sûr de vouloir supprimer cette offre? ")) {
+      this.store.dispatch(new offreActions.DeleteOffre(offre.id));
+    }
+  }
+
+  editCustomer(offre: Offre) {
+    this.store.dispatch(new offreActions.LoadOffre(offre.id));
+  }
 }
--- src/app/offres/state/offre.reducer.ts
+++ src/app/offres/state/offre.reducer.ts
@@ -2,23 +2,34 @@ import * as offreActions from "./offre.actions";
 import { Offre } from "../offre.model";
 import * as fromRoot from "../../state/app-state";
 
-export interface OffreState {
-    offres: Offre[],
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+
+import { EntityState, EntityAdapter, createEntityAdapter } from "@ngrx/entity";
+import { state } from "@angular/animations";
+
+export interface OffreState extends EntityState<Offre> {
+    selectedOffreId: number | null,
     loading: boolean,
     loaded: boolean,
     error: string
 }
 
 export interface AppState extends fromRoot.AppState{
-    offres: OffreState
+    offres: OffreState;
 }
 
-export const initialState: OffreState = {
-    offres: [],
+export const offreAdapter: EntityAdapter<Offre> = createEntityAdapter<Offre>();
+
+export const defaultOffre: OffreState = {
+    ids: [],
+    entities: {},
+    selectedOffreId: null,
     loading: false,
-    loaded:false,
+    loaded: false,
     error:""
 }
+
+export const initialState = offreAdapter.getInitialState(defaultOffre);
 
 export function offreReducer(state = initialState, action: offreActions.Actions): OffreState{
     switch(action.type){
@@ -28,17 +39,16 @@ case offreActions.OffreActionTypes.LOAD_OFFRES: {
         loading: true};
 }
 case offreActions.OffreActionTypes.LOAD_OFFRES_SUCCESS: {
-    return {
+    return offreAdapter.addAll(action.payload, {
         ...state,
         loading: false,
-        loaded: true,
-        offres: action.payload};
+        loaded: true});
 }
 
 case offreActions.OffreActionTypes.LOAD_OFFRES_FAIL:{
     return {
         ...state,
-        offres:[],
+        entities:{},
         loading:false,
         loaded: false,
         error: action.payload};
@@ -49,3 +59,36 @@ default: {
 }
 }
 }
+
+const getOffreFeatureState = createFeatureSelector<OffreState>(
+    "offres"
+)
+export const getOffres = createSelector(
+    getOffreFeatureState,
+    offreAdapter.getSelectors().selectAll
+  );
+  
+  export const getOffresLoading = createSelector(
+    getOffreFeatureState,
+    (state: OffreState) => state.loading
+  );
+  
+  export const getOffresLoaded = createSelector(
+    getOffreFeatureState,
+    (state: OffreState) => state.loaded
+  );
+  
+  export const getError = createSelector(
+    getOffreFeatureState,
+    (state: OffreState) => state.error
+  );
+  
+  export const getCurrentOffreId = createSelector(
+    getOffreFeatureState,
+    (state: OffreState) => state.selectedOffreId
+  );
+  export const getCurrentOffre = createSelector(
+    getOffreFeatureState,
+    getCurrentOffreId,
+    state => state.entities[state.selectedOffreId]
+  );
